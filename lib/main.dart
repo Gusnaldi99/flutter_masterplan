849e725c2fe01606d4db4c46410e3cf00ce3be61
Gusnaldi99 <gusnaldi.luthfi99@gmail.com>
2025-03-19 21:21:01 +0700

    Commit prak 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masterplan/Provider/plan_provider.dart';
 import 'package:flutter_masterplan/views/plan_screen.dart';
+import 'package:flutter_masterplan/Models/plan.dart';
 
-void main() => runApp(MasterPlanApp());
+void main() => runApp(const MasterPlanApp());
 
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
@@ -10,7 +12,10 @@ class MasterPlanApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: PlanScreen(),
+      home: PlanProvider(
+        child: const PlanScreen(),
+        notifier: ValueNotifier<List<Plan>>([const Plan()]),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/views/plan_screen.dart
+++ lib/views/plan_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masterplan/Models/data_layer.dart';
-import 'package:flutter_masterplan/Models/plan.dart';
+import 'package:flutter_masterplan/Provider/plan_provider.dart';
 
 class PlanScreen extends StatefulWidget {
   const PlanScreen({super.key});
@@ -10,8 +10,8 @@ class PlanScreen extends StatefulWidget {
 }
 
 class _PlanScreenState extends State<PlanScreen> {
-  Plan plan = const Plan();
   late ScrollController scrollController;
+
   @override
   void initState() {
     super.initState();
@@ -21,16 +21,23 @@ class _PlanScreenState extends State<PlanScreen> {
         });
   }
 
-  Widget _buildAddTaskButton() {
+  Widget _buildAddTaskButton(BuildContext context) {
+    // Ubah tipe data sesuai dengan yang dikembalikan oleh PlanProvider.of
+    ValueNotifier<List<Plan>> plansNotifier = PlanProvider.of(context);
     return FloatingActionButton(
       child: const Icon(Icons.add),
       onPressed: () {
-        setState(() {
-          plan = Plan(
-            name: plan.name,
-            tasks: List<Task>.from(plan.tasks)..add(const Task()),
+        // Asumsikan kita bekerja dengan plan pertama dalam list
+        List<Plan> currentPlans = plansNotifier.value;
+        if (currentPlans.isNotEmpty) {
+          Plan currentPlan = currentPlans[0];
+          List<Plan> newPlans = List<Plan>.from(currentPlans);
+          newPlans[0] = Plan(
+            name: currentPlan.name,
+            tasks: List<Task>.from(currentPlan.tasks)..add(const Task()),
           );
-        });
+          plansNotifier.value = newPlans;
+        }
       },
     );
   }
@@ -41,45 +48,51 @@ class _PlanScreenState extends State<PlanScreen> {
     super.dispose();
   }
 
-  Widget _buildList() {
+  Widget _buildList(Plan plan) {
     return ListView.builder(
       controller: scrollController,
-      keyboardDismissBehavior:
-          Theme.of(context).platform == TargetPlatform.iOS
-              ? ScrollViewKeyboardDismissBehavior.onDrag
-              : ScrollViewKeyboardDismissBehavior.manual,
       itemCount: plan.tasks.length,
-      itemBuilder: (context, index) => _buildTaskTile(plan.tasks[index], index),
+      itemBuilder:
+          (context, index) => _buildTaskTile(plan.tasks[index], index, context),
     );
   }
 
-  Widget _buildTaskTile(Task task, int index) {
+  Widget _buildTaskTile(Task task, int index, BuildContext context) {
+    ValueNotifier<List<Plan>> plansNotifier = PlanProvider.of(context);
     return ListTile(
       leading: Checkbox(
         value: task.complete,
         onChanged: (selected) {
-          setState(() {
-            plan = Plan(
-              name: plan.name,
-              tasks: List<Task>.from(plan.tasks)
+          List<Plan> currentPlans = plansNotifier.value;
+          if (currentPlans.isNotEmpty) {
+            Plan currentPlan = currentPlans[0];
+            List<Plan> newPlans = List<Plan>.from(currentPlans);
+            newPlans[0] = Plan(
+              name: currentPlan.name,
+              tasks: List<Task>.from(currentPlan.tasks)
                 ..[index] = Task(
                   description: task.description,
                   complete: selected ?? false,
                 ),
             );
-          });
+            plansNotifier.value = newPlans;
+          }
         },
       ),
       title: TextFormField(
         initialValue: task.description,
         onChanged: (text) {
-          setState(() {
-            plan = Plan(
-              name: plan.name,
-              tasks: List<Task>.from(plan.tasks)
+          List<Plan> currentPlans = plansNotifier.value;
+          if (currentPlans.isNotEmpty) {
+            Plan currentPlan = currentPlans[0];
+            List<Plan> newPlans = List<Plan>.from(currentPlans);
+            newPlans[0] = Plan(
+              name: currentPlan.name,
+              tasks: List<Task>.from(currentPlan.tasks)
                 ..[index] = Task(description: text, complete: task.complete),
             );
-          });
+            plansNotifier.value = newPlans;
+          }
         },
       ),
     );
@@ -88,10 +101,28 @@ class _PlanScreenState extends State<PlanScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ganti 'Namaku' dengan Nama panggilan Anda
-      appBar: AppBar(title: const Text('Master Plan Gusnaldi Luthfi')),
-      body: _buildList(),
-      floatingActionButton: _buildAddTaskButton(),
+      appBar: AppBar(title: const Text('Master Plan')),
+      body: ValueListenableBuilder<List<Plan>>(
+        valueListenable: PlanProvider.of(context),
+        builder: (context, plans, child) {
+          if (plans.isEmpty) {
+            return const Center(child: Text('No plans available'));
+          }
+
+          Plan plan = plans[0]; // Asumsikan kita bekerja dengan plan pertama
+          int completedCount = plan.tasks.where((task) => task.complete).length;
+          String completenessMessage =
+              '$completedCount out of ${plan.tasks.length} tasks';
+
+          return Column(
+            children: [
+              Expanded(child: _buildList(plan)),
+              SafeArea(child: Text(completenessMessage)),
+            ],
+          );
+        },
+      ),
+      floatingActionButton: _buildAddTaskButton(context),
     );
   }
 }
